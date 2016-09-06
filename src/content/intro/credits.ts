84ddfdf620e96bd79ba378cffcb9cfde860503fd
furti <daniel.furtlehner@gmx.net>
2016-09-06 18:51:55 +0200

    Optimized typescript config for content scripts

--- src/content/intro/credits.ts
+++ src/content/intro/credits.ts
@@ -1,3 +1,5 @@
+import { console } from '../console';
+
 namespace intro.credits {
     export function run(commandParams: console.CommandParams): void {
         commandParams.console.executeCommand('read credits.md');
